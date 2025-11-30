baa35214c6cb00bb52cdce58966ef6031854b5c6
Nitesh001 <nitesh.agarwal2008@gmail.com>
2025-11-30 22:45:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# AWS DevOps Dummy App
+
+- Run locally:
+  npm install
+  npm start
+
+- Or build docker:
+  docker build -t aws-devops-dummy-app .
+  docker run -p 3000:3000 aws-devops-dummy-app
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const os = require('os');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send({
+    message: "Hello from AWS DevOps Dummy App",
+    host: os.hostname(),
+    timestamp: new Date().toISOString()
+  });
+});
+
+app.get('/echo/:text', (req, res) => {
+  res.send({ echo: req.params.text });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
